32f5412566f0a27dfcf89a3bd961dbf00595c3f6
ajid2 <ajidalmajid6@gmail.com>
2021-02-11 16:21:51 +0700

    FIX(base_url): error base_url

---
+++ WPeopleAPI-main/Encryptor.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Bims;
+
+class Encryptor
+{
+
+    public static function EncryptorCipher($ch, $key)
+    {
+        if (!ctype_alpha($ch))
+            return $ch;
+
+        $offset = ord(ctype_upper($ch) ? 'A' : 'a');
+        return chr(fmod(((ord($ch) + $key) - $offset), 26) + $offset);
+    }
+
+    public static function EncryptorEncipher($input, $key)
+    {
+        $output = "";
+
+        $inputArr = str_split($input);
+        foreach ($inputArr as $ch)
+            $output .= self::EncryptorCipher($ch, $key);
+
+        return $output;
+    }
+
+    public static function EncryptorDecipher($input, $key)
+    {
+        return self::EncryptorEncipher($input, 26 - $key);
+    }
+
+    public static function EncryptorAbc($data)
+    {
+        $md5    = md5("$data" . 'Encryptor');
+        $az     = implode(range('a', 'z'));
+        $base64 = strtolower(base64_encode($md5)) . "$az";
+        $final  = preg_replace('([0-9|\=])', '', "$base64");
+        $arr    = array_unique(str_split($final));
+        return implode($arr);
+    }
+
+    public static function EncryptorABCD($data)
+    {
+        $md5    = md5("$data" . '_Encryptor');
+        $az     = implode(range('A', 'Z'));
+        $base64 = strtoupper(base64_encode($md5)) . "$az";
+        $final  = preg_replace('([0-9|\=])', '', "$base64");
+        $arr    = array_unique(str_split($final));
+        return implode($arr);
+    }
+
+    public static function encrypt($data, $key)
+    {
+        $keyLen = strlen($key);
+        $az     = implode(range('a', 'z'));
+        $AZ     = implode(range('A', 'Z'));
+        $rand   = self::EncryptorAbc($key);
+        $rand2  = self::EncryptorABCD($key);
+
+        $one    = base64_encode($data);
+        $two    = self::EncryptorEncipher($one, $keyLen);
+        $three  = strtr($two, $az, $rand);
+
+        $four   = strtr($three, $AZ, $rand2);
+        return $four;
+    }
+}
---
+++ WPeopleAPI-main/readme.txt
@@ -0,0 +1,28 @@
+=== WPeopleAPI ===
+Tags: google contact, people API
+Requires at least: 4.6
+Tested up to: 5.6
+Stable tag: 0.1.2
+License: GPLv2 or later
+
+A wordpress plugin to create contect in google contact using peopleAPI
+
+== Description ==
+
+Akismet checks your comments and contact form submissions against our global database of spam to prevent your site from publishing malicious content. You can review the comment spam it catches on your blog's "Comments" admin screen.
+
+Major features in Akismet include:
+
+* Automatically checks all comments and filters out the ones that look like spam.
+* Each comment has a status history, so you can easily see which comments were caught or cleared by Akismet and which were spammed or unspammed by a moderator.
+* URLs are shown in the comment body to reveal hidden or misleading links.
+* Moderators can see the number of approved comments for each user.
+* A discard feature that outright blocks the worst spam, saving you disk space and speeding up your site.
+
+PS: You'll be prompted to get an Akismet.com API key to use it, once activated. Keys are free for personal blogs; paid subscriptions are available for businesses and commercial sites.
+
+== Installation ==
+
+Upload the Akismet plugin to your blog, activate it, and then enter your Akismet.com API key.
+
+1, 2, 3: You're done!
