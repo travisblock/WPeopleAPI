bd88dbacfc4bf203601da51b8c3c96cf79f8294c
ajid2 <ajidalmajid6@gmail.com>
2021-02-11 16:26:50 +0700

    FIX(Filename):fix prefix

--- Filename.php
+++ Filename.php
@@ -4,7 +4,7 @@ namespace Bims;
 
 class Filename
 {
-    private static $prefix = NULL;
+    private static $prefix = "2c7a435da68e3d10668dee05066228a9ab21f56926c1d15f7583361f6ab5895b";
     public static function filename()
     {
         return self::$prefix;
