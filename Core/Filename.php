08f33399d68121408950f8845bd0c36b7b914d35
ajid2 <ajidalmajid6@gmail.com>
2021-02-15 15:27:09 +0700

    REFACTOR(Filename): Move Filename to core

--- Core/Filename.php
+++ Core/Filename.php
@@ -4,7 +4,7 @@ namespace Bims\Core;
 
 class Filename
 {
-    private static $prefix = "43cede15cf15c978251d0e8b76aa395ed8bb3da117b99c920f38bebb2ee765ba";
+    private static $prefix = NULL;
     public static function filename()
     {
         return self::$prefix;
